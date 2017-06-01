ac888efea75cc808b125ef09435c29d1300c2fe6
gaurav <gaurav@ggvaidya.com>
2017-06-01 19:09:27 -0400

    Added reversed values (compare to 'next' and 'last').

--- src/main/java/com/ggvaidya/scinames/summary/NameStabilityView.java
+++ src/main/java/com/ggvaidya/scinames/summary/NameStabilityView.java
@@ -26,6 +26,7 @@ package com.ggvaidya.scinames.summary;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.logging.Logger;
@@ -120,20 +121,34 @@ public final class NameStabilityView {
 		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_prev_pc_union"));		
 		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_prev"));
 		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_prev_pc_this"));
-		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_prev_pc_union"));		
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_prev_pc_union"));
+		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_next"));
+		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_next_pc_this"));
+		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_next_pc_union"));		
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_next"));
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_next_pc_this"));
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_next_pc_union"));	
 		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_first"));
 		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_first_pc_this"));
 		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_first_pc_union"));		
 		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_first"));
 		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_first_pc_this"));	
 		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_first_pc_union"));
+		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_last"));
+		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_last_pc_this"));
+		cols.add(createTableColumnFromPrecalc(precalc, "names_identical_to_last_pc_union"));		
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_last"));
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_last_pc_this"));	
+		cols.add(createTableColumnFromPrecalc(precalc, "clusters_identical_to_last_pc_union"));		
 		
-		Dataset firstDataset = null;
+		Dataset firstDataset = project.getDatasets().get(0);
+		Dataset lastDataset = project.getDatasets().get(project.getDatasets().size() - 1);
+		int index = -1;
 		for(Dataset ds: project.getDatasets()) {
-			if(firstDataset == null && ds != null)
-				firstDataset = ds;
+			index++;
 			
 			Dataset prevDataset = ds.getPreviousDataset();
+			Dataset nextDataset = (index < (project.getDatasets().size() - 1) ? project.getDatasets().get(index + 1) : null);
 			
 			precalc.put(ds, "dataset", ds.getName());
 			precalc.put(ds, "date", ds.getDate().toString());
@@ -189,6 +204,23 @@ public final class NameStabilityView {
 				precalc.put(ds, "clusters_identical_to_first_pc_union", new BigDecimal((double)getBinomialClustersIntersection(project, ds, firstDataset).size()/getBinomialClustersUnion(project, ds, firstDataset).size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
 			}
 			
+			if(lastDataset == null) {
+//				precalc.put(ds, "names_identical_to_first", "NA");
+//				precalc.put(ds, "names_identical_to_first_pc", "NA");
+			} else {
+				precalc.put(ds, "names_identical_to_last", String.valueOf(getBinomialNamesIntersection(project, ds, lastDataset).size()));
+				precalc.put(ds, "names_identical_to_last_pc_this", new BigDecimal((double)getBinomialNamesIntersection(project, ds, lastDataset).size()/recognizedBinomials.size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+				precalc.put(ds, "names_identical_to_last_pc_union", new BigDecimal((double)getBinomialNamesIntersection(project, ds, lastDataset).size()/getBinomialNamesUnion(project, ds, firstDataset).size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+
+				int clustersForDataset = project.getNameClusterManager().getClusters(recognizedBinomials).size();
+				if(clustersForDataset != recognizedBinomials.size()) {
+					throw new RuntimeException("We have " + clustersForDataset + " clusters for this dataset, but " + recognizedBinomials.size() + " recognized binomials. What?");
+				}
+				precalc.put(ds, "clusters_identical_to_last", String.valueOf(getBinomialClustersIntersection(project, ds, lastDataset).size()));
+				precalc.put(ds, "clusters_identical_to_last_pc_this", new BigDecimal((double)getBinomialClustersIntersection(project, ds, lastDataset).size()/clustersForDataset * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+				precalc.put(ds, "clusters_identical_to_last_pc_union", new BigDecimal((double)getBinomialClustersIntersection(project, ds, lastDataset).size()/getBinomialClustersUnion(project, ds, lastDataset).size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+			}
+			
 			if(prevDataset == null) {
 //				precalc.put(ds, "names_identical_to_prev", "NA");
 //				precalc.put(ds, "names_identical_to_prev_pc", "NA");				
@@ -204,6 +236,23 @@ public final class NameStabilityView {
 				precalc.put(ds, "clusters_identical_to_prev", String.valueOf(getBinomialClustersIntersection(project, ds, prevDataset).size()));
 				precalc.put(ds, "clusters_identical_to_prev_pc_this", new BigDecimal((double)getBinomialClustersIntersection(project, ds, prevDataset).size()/clustersForDataset * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
 				precalc.put(ds, "clusters_identical_to_prev_pc_union", new BigDecimal((double)getBinomialClustersIntersection(project, ds, prevDataset).size()/getBinomialClustersUnion(project, ds, prevDataset).size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+			}
+		
+			if(nextDataset == null) {
+	//			precalc.put(ds, "names_identical_to_prev", "NA");
+	//			precalc.put(ds, "names_identical_to_prev_pc", "NA");				
+			} else {
+				precalc.put(ds, "names_identical_to_next", String.valueOf(getBinomialNamesIntersection(project, ds, nextDataset).size()));
+				precalc.put(ds, "names_identical_to_next_pc_this", new BigDecimal((double)getBinomialNamesIntersection(project, ds, nextDataset).size()/recognizedBinomials.size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+				precalc.put(ds, "names_identical_to_next_pc_union", new BigDecimal((double)getBinomialNamesIntersection(project, ds, nextDataset).size()/getBinomialNamesUnion(project, ds, nextDataset).size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+				
+				int clustersForDataset = project.getNameClusterManager().getClusters(recognizedBinomials).size();
+				if(clustersForDataset != recognizedBinomials.size()) {
+					throw new RuntimeException("We have " + clustersForDataset + " clusters for this dataset, but " + recognizedBinomials.size() + " recognized binomials. What?");
+				}
+				precalc.put(ds, "clusters_identical_to_next", String.valueOf(getBinomialClustersIntersection(project, ds, nextDataset).size()));
+				precalc.put(ds, "clusters_identical_to_next_pc_this", new BigDecimal((double)getBinomialClustersIntersection(project, ds, nextDataset).size()/clustersForDataset * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
+				precalc.put(ds, "clusters_identical_to_next_pc_union", new BigDecimal((double)getBinomialClustersIntersection(project, ds, nextDataset).size()/getBinomialClustersUnion(project, ds, nextDataset).size() * 100).setScale(2, BigDecimal.ROUND_HALF_EVEN).toPlainString());
 			}
 		}
 		
@@ -233,8 +282,8 @@ public final class NameStabilityView {
 		Set<NameCluster> clusters1 = new HashSet<>(ncm.getClusters(p.getRecognizedNames(ds1).stream().flatMap(n -> n.asBinomial()).collect(Collectors.toList())));
 		Set<NameCluster> clusters2 = new HashSet<>(ncm.getClusters(p.getRecognizedNames(ds2).stream().flatMap(n -> n.asBinomial()).collect(Collectors.toList())));
 		
-		if(ds1 == ds2)
-			LOGGER.info("Present in " + ds1 + " but not in " + ds2 + ": " + clusters1.stream().filter(c -> !clusters2.contains(c)).map(c -> c.toString()).collect(Collectors.joining("; ")));
+		//if(ds1 == ds2)
+		//	LOGGER.info("Present in " + ds1 + " but not in " + ds2 + ": " + clusters1.stream().filter(c -> !clusters2.contains(c)).map(c -> c.toString()).collect(Collectors.joining("; ")));
 		
 		return clusters1.stream().filter(c -> clusters2.contains(c)).collect(Collectors.toSet());
 	}
